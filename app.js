a8bdd457715096b206f74fe457db454e1feca63b
Nemanja <nemanja94email@gmail.com>
2022-02-28 11:20:41 +0100

    create function to add book to the page

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-/* Add Books Button */
+/* Add Book Button */
 const addBtn = document.querySelector("#addBookBtn");
-/* Display Books */
+/* Display Books Container */
 const booksContainer = document.querySelector("#books");
 /* variable to store all books */
 const myLibrary = [];
@@ -20,23 +20,65 @@ class Book {
   }
 };
 
-/* add book function */
+/* Add book function */
 const addBook = () => {
-  const title = document.querySelector("#title").value;
-  const author = document.querySelector("#author").value;
-  const pages = document.querySelector("#pages").value;
-  const isRead = document.querySelector("#isRead").checked;
+  const title = document.querySelector("#title");
+  const author = document.querySelector("#author");
+  const pages = document.querySelector("#pages");
+  const isRead = document.querySelector("#isRead");
 
   /* Check for all fields */
-  if(!title || !author || !pages) return
+  if(!title.value || !author.value || !pages.value) return
 
   /* Add Book */
-  const newBook = new Book(title, author, pages, isRead);
-
+  const newBook = new Book(title.value, author.value, pages.value, isRead.checked);
+  /* Push the book to the library */
   myLibrary.push(newBook);
 
-  
+  /* Create HTML for the book and add it to the page */
+  const bookHtml = createHtmlBook(title.value, author.value, pages.value, isRead.checked);
+  booksContainer.appendChild(bookHtml);
+
+  /* Clear input fields */
+  title.value = "";
+  author.value = "";
+  pages.value = "";
+  isRead.checked = "";
 }
 
-/* Add Book Button */
+/* Add event listener to Book Button */
 addBtn.addEventListener("click", addBook);
+
+/* Create HTML for a Book */
+function createHtmlBook(title, author, pages, isRead) {
+  const bookWrapper = document.createElement("div");
+  bookWrapper.classList.add("book");
+
+  const bookTitle = document.createElement("h2");
+  bookTitle.classList.add("bookTitle");
+  bookTitle.textContent = title;
+
+  const bookAuthor = document.createElement("h5");
+  bookAuthor.classList.add("bookAuthor");
+  bookAuthor.textContent = `Author: ${author}`;
+
+  const bookPages = document.createElement("p");
+  bookPages.classList.add("bookPages");
+  bookPages.textContent = `Pages: ${pages}`;
+
+  const bookRead = document.createElement("p");
+  if(isRead) {
+    bookRead.classList.add("bookRead");
+    bookRead.textContent = "Read";
+  } else  {
+    bookRead.classList.add("bookNotRead");
+    bookRead.textContent = "Not Read";
+  }
+
+  bookWrapper.appendChild(bookTitle);
+  bookWrapper.appendChild(bookAuthor);
+  bookWrapper.appendChild(bookPages);
+  bookWrapper.appendChild(bookRead);
+
+  return bookWrapper;
+}
